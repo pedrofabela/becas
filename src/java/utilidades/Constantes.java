316299d22bdf157ad163b3c4f2ca7fa37917df1a
gioca <gioca@DESKTOP-QE1JR9D>
2019-06-03 12:48:58 -0500

    se agrego carpeta de reportes

--- src/java/utilidades/Constantes.java
+++ src/java/utilidades/Constantes.java
@@ -45,7 +45,7 @@ public class Constantes implements Serializable {
 		public static final boolean esDesarrollo = true;		
 		public static final String rutaProyectos =   "http://aplicaciones.edugem.gob.mx/";
 		public static final String rutareportesjasper = "/apachetomcat/reportes/";		                                                  
-		//public static final String rutaArch = "/tomcat/webapps/programas/permanecerestudiando/";  				
+		public static final String rutaArch = "/archivos";  				
 		public static final String rutaEstilos = "http://aplicaciones.edugem.gob.mx/estilosUDAI/";		                                        
 		public static final String rutaImages = "/tomcat/reportes/imagenes/";
 		public static final String rutaManuales = "http://aplicaciones.edugem.gob.mx/manuales/ManualUsuario3P.pdf";		
